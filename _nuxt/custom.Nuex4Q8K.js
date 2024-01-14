d286b158edf070a31bc9a84c9941acf075c0b8a0
TC Wang <wangtct@hotmail.com>
2024-01-14 23:13:13 +0800

    Updates

---
+++ _nuxt/custom.Nuex4Q8K.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./NavBar.tmzNvW2y.js";import{_ as c,c as n,b as s,U as r,o as a}from"./entry.bV0zAqMs.js";const _={},d={class:"container-fluid"};function f(o,l){const e=t;return a(),n("div",d,[s(e),r(o.$slots,"default")])}const p=c(_,[["render",f]]);export{p as default};
