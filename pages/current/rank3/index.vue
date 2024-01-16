6c468756c1b96e538c07fcbb20592f8bd61eed70
TC Wang <wangtct@hotmail.com>
2024-01-16 08:37:58 +0800

    blog

--- pages/current/rank3/index.vue
+++ pages/current/rank3/index.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="">
     <h1>享受當下 - 鳳</h1>
-    <button class="btn btn-primary m-2">
-      <a class="m-3 text-white" href="/current/rank3/blog">生活隨筆</a>
-    </button>
-    <button class="btn btn-primary m-2">
-      <a class="m-3 text-white" href="/current/rank3/album">相冊</a>
-    </button>
+
+    <table class="table table-sm table-responsive table-hover">
+      <thead>
+        <tr>
+          <th>年份/項目</th>
+          <th>地點</th>
+          <th>紀錄</th>
+          <th>照片</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>2024 加拿大旅遊</td>
+          <td>溫哥華 ＆ 蒙特利爾</td>
+
+          <td>
+            <button class="btn btn-primary m-2">
+              <a class="m-1 text-white" href="/current/rank3/blog">生活隨筆</a>
+            </button>
+          </td>
+          <td>
+            <button class="btn btn-primary m-2">
+              <a class="m-1 text-white" href="/current/rank3/album">相冊</a>
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
